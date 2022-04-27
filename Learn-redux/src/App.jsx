78150bb756030ca836601b668051bd1a6e153959
Rohit <stylishrb711@gmail.com>
2022-04-27 16:24:30 +0530

    redux added

--- Learn-redux/src/App.jsx
+++ Learn-redux/src/App.jsx
@@ -1,45 +1,40 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
-}
+    <>
+      <div className="main-div">
+        <div className="container">
+          <h1>Increment/Decrement counter</h1>
+          <h4>using React and Redux</h4>
 
-export default App
+          <div className="quantity">
+            <a
+              className="quantity__minus"
+              title="Decrement"
+              // onClick={() => dispatch(decNumber())}
+            >
+              <span>-</span>
+            </a>
+            <input
+              name="quantity"
+              type="text"
+              className="quantity__input"
+              // value={changeTheNumber}
+            />
+            <a
+              className="quantity__plus"
+              title="Increment"
+              // onClick={() => dispatch(incNumber(5))}
+            >
+              <span>+</span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
