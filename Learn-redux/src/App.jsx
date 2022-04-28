d4968a3fe8e88596340dbe9c514e3a0056697e2e
Rohit <stylishrb711@gmail.com>
2022-04-28 07:26:42 +0530

    redux app statrt working

--- Learn-redux/src/App.jsx
+++ Learn-redux/src/App.jsx
@@ -1,34 +1,38 @@
 import React from "react";
 import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
+import { incNumber, decNumber } from "./actions";
 
 const App = () => {
+  const myState = useSelector((state) => state.changeNumber);
+  const dispatch = useDispatch();
+
   return (
     <>
       <div className="main-div">
         <div className="container">
           <h1>Increment/Decrement counter</h1>
           <h4>using React and Redux</h4>
-
           <div className="quantity">
             <a
               className="quantity__minus"
               title="Decrement"
-              // onClick={() => dispatch(decNumber())}
+              onClick={() => dispatch(decNumber())}
             >
-              <span>-</span>
+              <span> - </span>
             </a>
             <input
               name="quantity"
               type="text"
               className="quantity__input"
-              // value={changeTheNumber}
+              value={myState}
             />
             <a
               className="quantity__plus"
               title="Increment"
-              // onClick={() => dispatch(incNumber(5))}
+              onClick={() => dispatch(incNumber())}
             >
-              <span>+</span>
+              <span> + </span>
             </a>
           </div>
         </div>
---
+++ Learn-redux/src/reducers/index.js
@@ -0,0 +1,9 @@
+import changeNumber from "./Counter";
+
+import { combineReducers } from "redux";
+
+const rootReducer = combineReducers({
+  changeNumber,
+});
+
+export default rootReducer;
