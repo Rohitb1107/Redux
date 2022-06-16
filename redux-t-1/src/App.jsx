a9f6bd02f4b5dbec882bb1d60e3c0f66a1cda2a0
Rohit <stylishrb711@gmail.com>
2022-06-16 08:30:15 +0530

    app update

--- redux-t-1/src/App.jsx
+++ redux-t-1/src/App.jsx
@@ -1,12 +1,10 @@
 import { useState } from "react";
 import "./App.css";
-import Intro from "./Components/Intro";
 
 function App() {
   return (
     <div className="App">
       <h1>Redux-1</h1>
-      <Intro/>
     </div>
   );
 }
--- redux-t-1/src/Components/Intro.js
+++ redux-t-1/src/Components/Intro.js
@@ -7,13 +7,36 @@ const actionMaker = {
 //InitState
 const initState = { count: 0 };
 
+//Reducer
+const reducer = (store1, actionMaker) => {
+  switch (action.type) {
+    case "ADD_COUNT": {
+      return {
+        ...state,
+        count: store1.count + action.payload,
+      };
+    }
+    default: {
+      return store1;
+    }
+  }
+};
+
 //Store
 class Store {
-  constructor(initState, reducer) {
-    (this.state = initState), (this.reducer = reducer);
+  constructor(reducer, initState) {
+    this.reducer = reducer;
+    this.state = initState;
   }
 
   getState() {
     return this.state;
   }
 }
+
+const store1 = new Store(initState, reducer);
+
+console.log(store1.getState());
+
+
+export default introJs;
